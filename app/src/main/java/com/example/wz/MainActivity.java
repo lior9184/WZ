a9dc8fbcfa318fab9e95c8e39fd61d3b05f47788
lior9184 <lior9184@gmail.com>
2021-01-11 15:36:28 +0200

    FIRST COMMIT

--- app/src/main/java/com/example/wz/MainActivity.java
+++ app/src/main/java/com/example/wz/MainActivity.java
@@ -1,14 +1,36 @@
 package com.example.wz;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.ButtonBarLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Button btn;
+    private TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+        tv = findViewById(R.id.tv);
+        btn.setOnClickListener(this);
+
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == btn)
+        {
+         tv.setText("Hello Yossi & Lior");
+        }
+
+
     }
 }
